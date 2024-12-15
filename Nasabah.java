873023409da803106642d7d117d8cd6e76d22b75
unproduktif <dodiwijayabusiness@gmail.com>
2024-12-15 20:47:48 +0800

    Valerine

---
+++ Nasabah.java
@@ -0,0 +1,15 @@
+public class Nasabah {
+    String nama;
+    String alamat;
+    double saldo;
+    String username;
+    String password;
+
+    public Nasabah(String nama, String alamat, String username, String password){
+        this.nama = nama;
+        this.alamat = alamat;
+        this.saldo = 0.0;
+        this.username = username;
+        this.password = password;
+    }
+}
