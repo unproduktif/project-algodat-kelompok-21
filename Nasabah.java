536c28c3d9ce24c28d8af537bd87986ef38f8312
Muhammad Ihdal Fahroni <ihdalfahroni728@gmail.com>
2024-12-15 15:47:32 +0800

    Update Nasabah.java

--- Nasabah.java
+++ Nasabah.java
@@ -1,15 +1,1 @@
-public class Nasabah {
-    String nama;
-    String alamat;
-    double saldo;
-    String username;
-    String password;
 
-    public Nasabah(String nama, String alamat, double saldo, String username, String password){
-        this.nama = nama;
-        this.alamat = alamat;
-        this.saldo = 0.0;
-        this.username = username;
-        this.password = password;
-    }
-}
