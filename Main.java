845f91c8ebe81abfaea98cb3a7bbb6e396aecf9a
unproduktif <dodiwijayabusiness@gmail.com>
2024-12-27 14:06:35 +0800

    last

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         LinkedListNasabah LLnasabah = new LinkedListNasabah();
+        LLnasabah.daftar("Valerine","Mataram","valerine","erin",370000);
+        LLnasabah.daftar("Dodi Wijaya","Kekalik Jaya","dodi","dod",150000);
+        LLnasabah.daftar("Agus Setiawan","Narmada","agus","agus123",200000);
         Scanner scanner = new Scanner(System.in);
         MenuAwal.tampilkanMenu(LLnasabah, scanner);
     }
